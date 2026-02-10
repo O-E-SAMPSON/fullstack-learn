306b4d2c79e2656367a6bebb3e180b3c8bfb3b2a
O-E-SAMPSON <sampsoneonyemaechi@gmail.com>
2026-02-10 07:03:47 +0100

    documents to use for learning

---
+++ script.js
@@ -0,0 +1,30 @@
+
+//let lastName = "sam";
+//lastName = "semeon";
+//console.log(lastName);
+
+//let age = 55;
+//age = 5;
+//console.log(age);
+
+
+//let isfemale = false;
+//isfemale = true;
+//console.log(isfemale);
+
+//const lastName = "sam";
+
+//console.log(lastName);
+
+//const age = 55;
+
+//console.log(age);
+
+
+//const isfemale = false;
+
+//console.log(isfemale);
+
+let firstName = "david";
+console.log(firstName.toUpperCase());
+console.log(firstName.length);
